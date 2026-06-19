ad788a8976da1aa35073f0113aa092958bbbb422
ligh <ligh1314@sina.com>
2026-06-19 16:20:50 +0900

    dev环境设置

--- mall-gateway/src/main/java/com/kubemall/gateway/config/GatewayConfig.java
+++ mall-gateway/src/main/java/com/kubemall/gateway/config/GatewayConfig.java
@@ -1,38 +1,36 @@
 package com.kubemall.gateway.config;
 
-import com.kubemall.gateway.filter.JwtAuthFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.kubemall.gateway.filter.JwtAuthFilter;
+
 @Configuration
 public class GatewayConfig {
 
-    private final JwtAuthFilter jwtAuthenticationFilter;
+        private final JwtAuthFilter jwtAuthenticationFilter;
 
-    public GatewayConfig(JwtAuthFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
+        public GatewayConfig(JwtAuthFilter jwtAuthenticationFilter) {
+                this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        }
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-
-                .route("product-service", r -> r
-                        .path("/api/v1/products/**")
-                        .filters(f -> f
-                                .filter(jwtAuthenticationFilter)
-                                .stripPrefix(2))
-                        .uri("http://mall-product:8082"))
-
-                .route("user-service", r -> r
-                        .path("/api/v1/**")
-                        .filters(f -> f
-                                .filter(jwtAuthenticationFilter)
-                                .stripPrefix(2))
-                        .uri("http://mall-user:8081"))
-
-                .build();
-    }
+        @Bean
+        public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route("product-service", r -> r
+                                                .path("/api/v1/products/**")
+                                                .filters(f -> f
+                                                                .filter(jwtAuthenticationFilter)
+                                                                .stripPrefix(2))
+                                                .uri("http://localhost:8082"))
+                                .route("user-service", r -> r
+                                                .path("/api/v1/**")
+                                                .filters(f -> f
+                                                                .filter(jwtAuthenticationFilter)
+                                                                .stripPrefix(2))
+                                                .uri("http://localhost:8081"))
+                                .build();
+        }
 }
